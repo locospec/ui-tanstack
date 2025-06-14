238048207bd242911f435fb3ea5fe9d44752c345
Rajiv Seelam <rajiv@betalectic.com>
2025-06-14 16:38:52 +0530

    feat: add component registry with card and loader implementation

--- src/routes/users.tsx
+++ src/routes/users.tsx
@@ -1,6 +1,11 @@
+import ComponentCard from "@/components/component-card";
+import ComponentLoader from "@/components/component-loader-server";
 import { Button } from "@/components/ui/button";
+import registry from "@/registry.json";
 import { Link, Outlet, createFileRoute } from "@tanstack/react-router";
+import type { RegistryItem } from "shadcn/registry";
 import type { User } from "../utils/users";
+const allComponents = registry.items as unknown as RegistryItem[];
 
 export const Route = createFileRoute("/users")({
   loader: async () => {
@@ -17,12 +22,36 @@ export const Route = createFileRoute("/users")({
   component: UsersComponent,
 });
 
+export const getComponentsByNames = (names: string[]): RegistryItem[] => {
+  const componentsMap = new Map(allComponents.map(comp => [comp.name, comp]));
+
+  return names
+    .map(name => componentsMap.get(name))
+    .filter((comp): comp is RegistryItem => comp !== undefined);
+};
+
 function UsersComponent() {
   const users = Route.useLoaderData();
+
+  const components = getComponentsByNames(allComponents.map(item => item.name));
+
+  console.log("registry", components);
 
   return (
     <div className="flex gap-2 bg-red-200 p-2">
       <Button>Button</Button>
+
+      {components.map(component => (
+        <ComponentCard
+          key={component.name}
+          component={component}
+          className="data-[slot=comp-542]:px-0"
+        >
+          <ComponentLoader component={component} />
+          {/* <ComponentDetails component={component} /> */}
+        </ComponentCard>
+      ))}
+
       <ul className="list-disc pl-4">
         {[
           ...users,
