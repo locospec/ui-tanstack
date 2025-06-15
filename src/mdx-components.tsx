ff25e29d082a86092bf4cf3c9694577af96244f1
Rajiv Seelam <rajiv@betalectic.com>
2025-06-15 18:51:11 +0530

    feat: add Button component to MDX content and register in mdx-components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Button,
     ...components,
   };
 }
