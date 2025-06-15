301895c48031e599ea04548d2349ab086cbfe5cc
Rajiv Seelam <rajiv@betalectic.com>
2025-06-15 19:23:18 +0530

    feat: add GithubCodeBlock component for rendering code snippets from GitHub URLs

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,3 +1,4 @@
+import GithubCodeBlock from "@/components/github-code-block";
 import { Button } from "@/components/ui/button";
 import { Accordion, Accordions } from "fumadocs-ui/components/accordion";
 import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
@@ -8,6 +9,7 @@ import type { MDXComponents } from "mdx/types";
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    GithubCodeBlock,
     Accordion,
     Accordions,
     Step,
