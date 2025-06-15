a3001110bc6f330053f78b089cbebab32d3502a2
Rajiv Seelam <rajiv@betalectic.com>
2025-06-15 19:02:55 +0530

    feat: add Fumadocs UI components and demo content with code samples

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,10 +1,22 @@
 import { Button } from "@/components/ui/button";
+import { Accordion, Accordions } from "fumadocs-ui/components/accordion";
+import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
+import { Step, Steps } from "fumadocs-ui/components/steps";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Accordion,
+    Accordions,
+    Step,
+    Steps,
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock keepBackground {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
     Button,
     ...components,
   };
